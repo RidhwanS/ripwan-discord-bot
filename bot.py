92a5de1b08b7f8004b0afd5567d0b545f708a721
Ridhwan Samadmoten <RidhwanSamad@gmail.com>
2018-03-21 22:15:02 -0400

    add camp command and file

--- bot.py
+++ bot.py
@@ -21,7 +21,12 @@ async def on_message(message):
         else:
             search = wikipedia.search(args)
             result = wikipedia.page(search[0])
-            em = discord.Embed(title='Wikipedia: ' + result.title, description=result.summary[:500] + '..', color=0x2db5ef, url=result.url)
+            em = discord.Embed(title='Wikipedia: ' + result.title, description=(result.summary[:500] + '..') if len(result.summary) >= 500 else result.summary, color=0x2db5ef, url=result.url)
             await client.send_message(message.channel, embed=em)
+    if message.content.lower().startswith('!camp'):
+        file_object = open("campInfo.txt", "r")
+        em = discord.Embed(title='Campaign Info', description=file_object.read(), color=0x2db5ef)
+        await client.send_message(message.channel, embed=em)
+
 
 client.run("<TOKEN>") #Insert your bots token here
