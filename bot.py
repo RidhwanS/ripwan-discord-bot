ac05193d1a5c6a5a76f01d0d0718d9de388c86bf
Ridhwan Samadmoten <ridhwan.samadmoten@gmail.com>
2018-10-25 00:14:45 -0400

    changed format and added strawpoll creation

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import discord
 from discord.ext.commands import Bot
-from discord.ext import commands
 import asyncio
 import time
 import wikipedia
 import random
+import requests
+import json
 
-Client = discord.Client()
-client = commands.Bot(command_prefix="!")
+client = Bot(command_prefix='!')
 
 eightBalls = {0: 'It is certain',
            1: 'Reply hazy try again',
@@ -43,41 +43,54 @@ eightBallsColours = {0: 0x00FF00,
            14: 0xFF0000
 }
 
-@client.event
-async def on_ready():
-    print("Bot is online and connected to Discord")
+@client.command(pass_context=True)
+async def wiki(ctx,*args):
+	search = wikipedia.search(' '.join(args))
+	try:
+		result = wikipedia.page(search[0])
+	except wikipedia.exceptions.DisambiguationError as e:
+		await client.say("The stuff you tried to search for had like a billion results. And wikipedia library doesnt like that or something. So RIP")
+		return
+	em = discord.Embed(title='Wikipedia: ' + result.title, description=(result.summary[:500] + '..') if len(result.summary) >= 500 else result.summary, color=0x2db5ef, url=result.url)
+	await client.say(embed=em)
 
-@client.event
-async def on_message(message):
-    if message.content.lower().startswith('!wiki'):
-        args = message.content.replace('!wiki', '', 1)
-        if len(args) == 0:
-            await client.send_message(message.channel, "No Message Was Next To Command")
-        else:
-            search = wikipedia.search(args)
-            try:
-                result = wikipedia.page(search[0])
-            except wikipedia.exceptions.DisambiguationError as e:
-                await client.send_message(message.channel, "The stuff you tried to search for had like a billion results. And wikipedia library doesnt like that or something. So RIP")
-            em = discord.Embed(title='Wikipedia: ' + result.title, description=(result.summary[:500] + '..') if len(result.summary) >= 500 else result.summary, color=0x2db5ef, url=result.url)
-            await client.send_message(message.channel, embed=em)
-    if message.content.lower().startswith('!camp'):
-        file_object = open("Files/campInfo.txt", "r")
-        em = discord.Embed(title='Campaign Info', description=file_object.read(), color=0x2db5ef)
-        await client.send_message(message.channel, embed=em)
-    if message.content.lower().startswith('!gitgud'):
-        number = random.randint(1, 6)
-        imageURL = "Images/gitgud-"+str(number)+".png"
-        with open(imageURL, 'rb') as f:
-            await client.send_file(message.channel, f, content="GIT GUD")
-    if message.content.lower().startswith('!8ball'):
-        args = message.content[6:]
-        number = random.randint(0, 14)
-        em = discord.Embed(title='8Ball:' + args, description=eightBalls[number], color=eightBallsColours[number])
-        await client.send_message(message.channel, embed=em)
-    if message.content.lower().startswith('!help'):
-        file_object = open("README.md", "r")
-        em = discord.Embed(title='Help', description=file_object.read(), color=0x2db5ef)
-        await client.send_message(message.channel, embed=em)
+@client.command()
+async def camp():
+	file_object = open("Files/campInfo.txt", "r")
+	em = discord.Embed(title='Campaign Info', description=file_object.read(), color=0x2db5ef)
+	await client.say(embed=em)
 
+@client.command(pass_context=True)
+async def gitgud(ctx):
+	number = random.randint(1, 6)
+	imageURL = "Images/gitgud-"+str(number)+".png"
+	#with open(imageURL, 'rb') as f:
+	await client.send_file(ctx.message.channel,imageURL)
+
+@client.command(name='8ball',pass_context=True)
+async def eight_ball(ctx,*args):
+	em = discord.Embed(title='8Ball: ' + ' '.join(args), description=random.choice(eightBalls), color=random.choice(eightBallsColours))
+	await client.say(embed=em)
+
+@client.command()
+async def poll(*args):
+	poll_parameters = list(args)
+	question = poll_parameters.count('Q:')
+	number_of_answers = poll_parameters.count('A:')
+	if question == 0 or question > 1 or number_of_answers == 0:
+		await client.say('The Format is, !poll Q: A Question A: Foo A: Bar. Only one Q: and at least one A:')
+		return
+	question_index = poll_parameters.index('Q:')
+	if question_index != 0:
+		await client.say('The Format is, !poll Q: A Question A: Foo A: Bar. The Q: has to be first')
+		return
+	first_answer_index = poll_parameters.index('A:')
+	question_string = ' '.join(poll_parameters[1:first_answer_index])
+	what_remains = poll_parameters[first_answer_index:]
+	answers_list = [i for i in ' '.join(what_remains).split('A: ')]
+	answers_list = answers_list[1:]
+	data = {"title": question_string, "options": answers_list}
+	poll = requests.post("https://www.strawpoll.me/api/v2/polls", json=data, headers={"Content-Type": "application/json"})
+	await client.say("https://www.strawpoll.me/" + str(json.loads(poll.text)['id']))
+	
 client.run("<token>") #Insert your bots token here
